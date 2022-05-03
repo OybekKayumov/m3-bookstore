b08cc7e2e9cc59f57cfe0d0a11643677c775e7f2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-03 06:05:49 +0500

    add redux categories.js and update congigureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/Books';
+import categoriesReducer from './categories/categories';
 
 const reducer = combineReducers({
   booksReducer,
+  categoriesReducer,
 });
 
 const store = createStore(
