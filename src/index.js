cc6a0259d1ed0d0a9dad8dbba0e1af01eec4c03c
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-04 08:31:15 +0500

    add fetch api, add remove item

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './redux/configureStore';
+import { getBook } from './redux/books/Books';
+
+store.dispatch(getBook);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/Books';
 import categoriesReducer from './categories/categories';
 
@@ -10,7 +11,7 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  applyMiddleware(logger),
+  applyMiddleware(logger, thunk),
 );
 
 export default store;
